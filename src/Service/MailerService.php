de83cca9caacfac7b23529a0a9ef00a0167aa407
Vladislav <131362319+vladSHOKO@users.noreply.github.com>
2025-05-27 19:34:29 +0300

    Delete unnecessary use statement

--- src/Service/MailerService.php
+++ src/Service/MailerService.php
@@ -5,7 +5,6 @@ namespace App\Service;
 use App\DTO\MailFormDTO;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
 
 class MailerService
@@ -26,7 +25,9 @@ class MailerService
             ->from('vladislavkuzminov111@gamail.com')
             ->to($formDTO->senderEmail)
             ->subject('Обратная связь')
-            ->text('Здравствуйте, ' . $formDTO->senderName . '! Я отвечу вам в течение некоторого времени! Если хотите ускорить процесс пишите в телеграм https://t.me/vladislavKuzminov2000');
+            ->text(
+                'Здравствуйте, ' . $formDTO->senderName . '! Я отвечу вам в течение некоторого времени! Если хотите ускорить процесс пишите в телеграм https://t.me/vladislavKuzminov2000'
+            );
 
         $this->mailer->send($email);
     }
